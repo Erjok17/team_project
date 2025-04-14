9b5da4e19fee1ecbf896e42145ab58ffd213332a
Erjok17 <mrerjok@gmail.com>
2025-04-14 12:41:05 +0300

    we back

--- team_project/src/middleware/authenticate.js
+++
@@ -1,8 +0,0 @@
-const isAuthenticated = (req, res, next) => {
-    if (req.session.user) {
-      return next();
-    }
-    res.status(401).json({ message: 'Unauthorized - Please log in first' });
-  };
-  
-  module.exports = isAuthenticated;
